87b0a09a6c23e17814da1c0a53c0e7d5368e683c
rthpm <rthp@fastmail.com>
2021-07-12 23:47:05 +0200

    Added a capitalized translation functionnality and its test

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -3,18 +3,24 @@ def translate(arg)
 end
 
 def peggy(word)
-  cons = %w[b c d f g h j k l m n p r s t v w x z]
-  if word.downcase.start_with? 'a', 'i', 'e', 'o', 'u'
-    "#{word}ay"
-  # elsif word.downcase.start_with? cons.combination(3).to_a.map(&:join).to_s
-  #  "#{word.chars.rotate(3).join}ay"
-  # elsif word.downcase.start_with? cons.combination(2).to_a.map(&:join).to_s
-  #  "#{word.chars.rotate(2).join}ay"
-  elsif word.downcase.start_with? 'sch', 'squ', 'thr'
-    "#{word.chars.rotate(3).join}ay"
-  elsif word.downcase.start_with? 'br', 'ch', 'qu', 'th'
-    "#{word.chars.rotate(2).join}ay"
+  # cons = %w[b c d f g h j k l m n p r s t v w x z]
+  @word = word.downcase
+  pig = if @word.start_with? 'a', 'i', 'e', 'o', 'u'
+          "#{@word}ay"
+        # elsif word.downcase.start_with? cons.combination(3).to_a.map(&:join).to_s
+        #  "#{word.chars.rotate(3).join}ay"
+        # elsif word.downcase.start_with? cons.combination(2).to_a.map(&:join).to_s
+        #  "#{word.chars.rotate(2).join}ay"
+        elsif @word.start_with? 'sch', 'squ', 'thr'
+          "#{@word.chars.rotate(3).join}ay"
+        elsif @word.start_with? 'br', 'ch', 'qu', 'th'
+          "#{@word.chars.rotate(2).join}ay"
+        else
+          "#{@word.chars.rotate(1).join}ay"
+        end
+  if word.capitalize == word
+    pig.capitalize
   else
-    "#{word.chars.rotate(1).join}ay"
+    pig
   end
 end
